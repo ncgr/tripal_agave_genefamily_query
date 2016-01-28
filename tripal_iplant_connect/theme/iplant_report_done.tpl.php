0768347cd866044e5df07cbbea4f583535301eab
simbiot <bu@ncgr.org>
2016-01-28 09:43:47 -0700

    Final updates

--- tripal_iplant_connect/theme/iplant_report_done.tpl.php
+++ tripal_iplant_connect/theme/iplant_report_done.tpl.php
@@ -9,14 +9,22 @@
 
 $path = $_SERVER['REQUEST_URI'];
 $job_id = basename($path);
-$resFile = "/tmp/job_$job_id.txt";
+$resFile = "/tmp/iplant_$job_id/results.txt";
 
 $myfile = fopen($resFile, "r") or die ("<br><p>Unable to access results in file $resFile</p>");
+print "Location: /tmp/iplant_$job_id<br>";
 while (!feof($myfile)) {
 	$line = fgets($myfile);
 	print "$line<br>";
 }
 fclose($myfile);
+
+$host = gethostname();
+$ipaddr = gethostbyname($host);
+
+print "<a href=\"http://velarde.ncgr.org:7070/isys/launch?svc=org.ncgr.isys.ncgr.jalview.JalviewAlignmentDisplayService%40http://$ipaddr/sites/default/files/iplant_$job_id/mafft_out.fa\">Launch JalView</a><br>";
+
+print "<a href=\"http://$ipaddr/sites/default/files/iplant_$job_id/mafft_out.fa\">Display File</a><br>\n";
 
 ?>
 
--- tripal_iplant_connect/theme/iplant_report_pending.tpl.php
+++ tripal_iplant_connect/theme/iplant_report_pending.tpl.php
@@ -45,10 +45,40 @@ Drupal.behaviors.blastuiSetTimeout = {
   <p>Your iPlant job is currently running. The results will be listed here as soon as it completes. This page will automatically refresh.</p>
 
 <?php
+$path = $_SERVER['REQUEST_URI'];
+$job_id = basename($path);
+$resFile = "/tmp/iplant_$job_id/err.log";
+$lineFile = "/tmp/iplant_$job_id/line_number.txt";
+
+$myfile = fopen($resFile, "r") or die ("<br><p>Unable to access results in file $resFile</p>");
+while (!feof($myfile)) {
+	$line = fgets($myfile);
+	$splitLine = explode(":", $line);
+	if ($splitLine[0] == "MESSAGE") {
+		$lastLine = $line;
+	}
+}
+fclose($myfile);
+
+$splitLine = explode(":", $lastLine);
+print ("<p><h2>Process: $splitLine[1]</h2><h2>Status: $splitLine[2]</h2>");
+
   }
   // JOB CANCELLED
   elseif ($status_code === 999) {
     drupal_set_title('iPlant Job Cancelled');
+
+$path = $_SERVER['REQUEST_URI'];
+$job_id = basename($path);
+$resFile = "/tmp/iplant_$job_id/err.log";
+
+$myfile = fopen($resFile, "r") or die ("<br><p>Unable to access results in file $resFile</p>");
+print "Location: /tmp/iplant_$job_id<br>";
+while (!feof($myfile)) {
+	$line = fgets($myfile);
+	print "$line<br>";
+}
+fclose($myfile);
 ?>
 
   <p>Unfortunately your iPlant job has been cancelled by an Administrator.  This page will automatically refresh.</p>
